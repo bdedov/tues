b160d42188dde63c696f4724a788df7c2631f524
Denislav Ivanov <denislavivanov82@gmail.com>
2021-07-07 08:47:06 +0300

    Fixed build system

--- car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/CloudConnection.java
+++ car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/CloudConnection.java
@@ -1,12 +1,18 @@
 package com.example.car_crash_assistant;
 
+import com.cumulocity.model.authentication.CumulocityBasicCredentials;
+import com.cumulocity.sdk.client.Platform;
+import com.cumulocity.sdk.client.PlatformImpl;
+
 public class CloudConnection
 {
     String tenant_id, username, password;
 
     public static boolean connect(String tenant_id, String username, String password)
     {
-        //TODO sockets :'(
+        
+
+
         return false;
     }
 
