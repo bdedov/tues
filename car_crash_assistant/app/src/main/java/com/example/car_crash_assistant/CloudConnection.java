75fc0aa8e757b5ea5b3132f79e8bdbdfaa7e7794
Denislav Ivanov <denislavivanov82@gmail.com>
2021-07-08 15:48:38 +0300

    added login check

--- car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/CloudConnection.java
+++ car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/CloudConnection.java
@@ -3,11 +3,13 @@ package com.example.car_crash_assistant;
 import android.content.Context;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
@@ -20,10 +22,12 @@ public class CloudConnection
     private static String auth_token;
     private static RequestQueue queue;
 
-    public static void create(String username, String password, Context context)
+    public static boolean create(String username, String password, Context context)
     {
         generateToken(username, password);
         queue = Volley.newRequestQueue(context);
+
+        return check_credentials();
     }
 
     private static void generateToken(String username, String password) {
@@ -59,6 +63,38 @@ public class CloudConnection
             };
 
         queue.add(jsonRequest);
+    }
+
+    private static boolean check_credentials()
+    {
+        int[] response_code = new int[1];
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://bdedov.1.stage.c8y.io/platform",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                response_code[0] = response.statusCode;
+                return super.parseNetworkResponse(response);
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "Basic " + auth_token);
+                return params;
+            }
+        };
+
+        return response_code[0] != 401;
     }
 
 //    public static void set_alarm(String severity)
