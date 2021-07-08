79255b34dd552bd154a62ecbfa141ab9f70c076e
Denislav Ivanov <denislavivanov82@gmail.com>
2021-07-08 12:59:10 +0300

    Added Measurement.java

--- car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/Measurement.java
+++ car_crash_assistant/app/src/main/java/com/example/car_crash_assistant/Measurement.java
@@ -2,20 +2,21 @@ package com.example.car_crash_assistant;
 
 import org.json.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Measurement
 {
+    private final static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
     public static void send(float[] acceleration) throws JSONException //will accept coordinates l8r
     {
-        //DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ");
         JSONObject measurement = new JSONObject();
         JSONObject source = new JSONObject();
         JSONObject c8y_Acceleration = new JSONObject();
-        JSONObject c8y_Location = new JSONObject();
         JSONObject a_x = new JSONObject();
         JSONObject a_y = new JSONObject();
         JSONObject a_z = new JSONObject();
-        JSONObject longitude = new JSONObject();
-        JSONObject latitude = new JSONObject();
 
         source.put("id", "3362");
         a_x.put("value", acceleration[0]);
@@ -25,15 +26,15 @@ public class Measurement
         a_z.put("value", acceleration[2]);
         a_z.put("unit", "g");
 
-        c8y_Acceleration.put("x", a_x);
-        c8y_Acceleration.put("y", a_y);
-        c8y_Acceleration.put("z", a_z);
+        c8y_Acceleration.put("accelerationX", a_x);
+        c8y_Acceleration.put("accelerationY", a_y);
+        c8y_Acceleration.put("accelerationZ", a_z);
 
-        measurement.put("time", "2011-09-19T12:03:27.845Z");
+        measurement.put("time", formatter.format(new Date()));
         measurement.put("type", "c8y_Acceleration");
         measurement.put("source", source);
         measurement.put("c8y_Acceleration", c8y_Acceleration);
 
-        //CloudConnection.connect();
+        CloudConnection.post_measurement(measurement);
     }
 }
